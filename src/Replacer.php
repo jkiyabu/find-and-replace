3c138f26d87e956ad5ff24959a408b84197cef2e
Jayeson Kiyabu and Zach Swanson <zach@>
2017-02-15 10:54:32 -0800

    add find and replacement of full words and partial words

--- src/Replacer.php
+++ src/Replacer.php
@@ -2,16 +2,44 @@
 
     class Replacer
     {
-        private initialPhrase;
-        private searchString;
-        private replacementString;
-        private resultPhrase;
+        private $initialPhrase;
+        private $searchString;
+        private $replacementString;
+        private $resultPhrase;
 
         function __construct($initialPhrase,$searchString,$replacementString)
         {
             $this->initialPhrase = $initialPhrase;
             $this->searchString = $searchString;
             $this->replacementString = $replacementString;
+        }
+
+
+        function findAndReplaceWhole()
+        {
+            $words = explode(" ", $this->initialPhrase);
+            foreach ($words as &$word) {
+                if (strtolower($word) == strtolower($this->searchString)) {
+                    $word = $this->replacementString;
+                }
+            }
+            unset($word);
+            $words = implode(" ", $words);
+            return $words;
+        }
+
+
+        function findAndReplacePartial()
+        {
+            $words = explode(" ", $this->initialPhrase);
+            foreach ($words as &$word) {
+                if (stristr($word,$this->searchString)) {
+                    $word = str_replace(strtolower($this->searchString), $this->replacementString, strtolower($word));
+                }
+            }
+            unset($word);
+            $words = implode(" ", $words);
+            return $words;
         }
 
     }
--- tests/replacementTest.php
+++ tests/replacementTest.php
@@ -3,11 +3,31 @@
 
     class ReplacerTest extends PHPUnit_Framework_TestCase
     {
-        
+        function test_WholeWord()
+        {
+            // Arrange
+            $initialPhrase = "I am walking my Cat to the cathedral";
+            $searchString = "cat";
+            $replacementString = "dog";
+            $test_case = new Replacer($initialPhrase,$searchString,$replacementString);
+            // Act
+            $result = $test_case->findAndReplaceWhole();
+            // Assert
+            $this->assertEquals("I am walking my dog to the cathedral", $result);
+        }
 
-
-
-
+        function test_PartialWord()
+        {
+            // Arrange
+            $initialPhrase = "I am walking my Cat to the cathedral";
+            $searchString = "cat";
+            $replacementString = "dog";
+            $test_case = new Replacer($initialPhrase,$searchString,$replacementString);
+            // Act
+            $result = $test_case->findAndReplacePartial();
+            // Assert
+            $this->assertEquals("I am walking my dog to the doghedral", $result);
+        }
 
 
     }
